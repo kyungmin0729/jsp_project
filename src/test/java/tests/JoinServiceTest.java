fc5b6be498bec3da4f4af44a4a9fd98b0811a0bc
kyungmin0729 <kyungmin0729@naver.com>
2023-10-20 15:24:14 +0900

    ("아이디(6자리 이상), 비밀번호(8자리 이상) 최소 자리수 체크, 실패시 BadRequestException 발생") 만듬

--- src/test/java/tests/JoinServiceTest.java
+++ src/test/java/tests/JoinServiceTest.java
@@ -43,70 +43,84 @@ public class JoinServiceTest {
     void requiredFieldCheck() {
         assertAll(
                 () -> {
-                    //아이디 검증(userId)
+                    // 아이디 검증(userId)
                     Member member = getMember();
                     member.setUserId(null);
-                    requiredFieldEachCheck(member, "아이디");
+                    fieldEachCheck(member, "아이디");
 
-                    //비어있는 값 검증
-                    member.setUserId("   ");
-                    requiredFieldEachCheck(member, "아이디");
+                    member.setUserId("  ");
+                    fieldEachCheck(member, "아이디");
                 },
                 () -> {
-                    //비밀번호 검증(userPw)
+                    // 비밀번호 검증(userPw)
                     Member member = getMember();
                     member.setUserPw(null);
-                    requiredFieldEachCheck(member, "비밀번호");
+                    fieldEachCheck(member, "비밀번호");
 
-                    //비어있는 값 검증
-                    member.setUserPw("   ");
-                    requiredFieldEachCheck(member, "비밀번호");
+                    member.setUserPw("  ");
+                    fieldEachCheck(member, "비밀번호");
                 },
                 () -> {
-                    //비밀번호 확인 검증(confirmUserPw)
+                    // 비밀번호 확인(confirmUserPw)
                     Member member = getMember();
                     member.setConfirmUserPw(null);
-                    requiredFieldEachCheck(member, "비밀번호를 확인");
+                    fieldEachCheck(member, "비밀번호를 확인");
 
-                    //비어있는 값 검증
-                    member.setConfirmUserPw("   ");
-                    requiredFieldEachCheck(member, "비밀번호를 확인");
+                    member.setConfirmUserPw("  ");
+                    fieldEachCheck(member, "비밀번호를 확인");
                 },
                 () -> {
-                    //회원명 검증(userNm)
+                    // 회원명 검증(userNm)
                     Member member = getMember();
                     member.setUserNm(null);
-                    requiredFieldEachCheck(member, "회원명");
+                    fieldEachCheck(member, "회원명");
 
-                    //비어있는 값 검증
-                    member.setUserNm("   ");
-                    requiredFieldEachCheck(member, "회원명");
+                    member.setUserNm("  ");
+                    fieldEachCheck(member, "회원명");
                 },
                 () -> {
-                    //이메일 검증(email)
+                    // 이메일 검증(email)
                     Member member = getMember();
                     member.setEmail(null);
-                    requiredFieldEachCheck(member, "이메일");
+                    fieldEachCheck(member, "이메일");
 
-                    //비어있는 값 검증
-                    member.setEmail("   ");
-                    requiredFieldEachCheck(member, "이메일");
+                    member.setEmail("  ");
+                    fieldEachCheck(member, "이메일");
                 },
                 () -> {
-                    //약관동의 검증(agree)
+                    // 약관 동의 검증(agree)
                     Member member = getMember();
                     member.setAgree(false);
-                    requiredFieldEachCheck(member, "약관");
-                });
+                    fieldEachCheck(member, "약관");
+                }
+        );
+
     }
 
-
-
-    private void requiredFieldEachCheck(Member member, String word) {
+    private void fieldEachCheck(Member member, String word) {
         BadRequestException thrown = assertThrows(BadRequestException.class, () -> {
             joinService.join(member);
         });
+
         assertTrue(thrown.getMessage().contains(word));
+
     }
-    // - 필수 항목 검증(아이디, 비밀번호, 비밀번호 확인, 회원명, 이메일, 회원가입약관 동의) 완료
+
+    @Test
+    @DisplayName("아이디(6자리 이상), 비밀번호(8자리 이상) 최소 자리수 체크, 실패시 BadRequestException 발생")
+    void fieldLengthCheck() {
+        assertAll(
+                () -> {
+                    // 아이디 6자리 이상 검증
+                    Member member = getMember();
+                    member.setUserId("user");
+                    fieldEachCheck(member, "아이디는 6자리");
+                },
+                () -> {
+                    // 비밀번호 8자리 이상 검증
+                    Member member = getMember();
+                    member.setUserPw("1234");
+                    fieldEachCheck(member, "비밀번호는 8자리");
+                });
+    }
 }
