3c1ab4c81c1f6855fdd315ee7ee681e90352249d
kyungmin0729 <kyungmin0729@naver.com>
2023-10-20 14:43:13 +0900

    정확하게 확인하기 위하여 문구에 관한 검증 추가

--- src/test/java/tests/JoinServiceTest.java
+++ src/test/java/tests/JoinServiceTest.java
@@ -10,46 +10,68 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("회원가입 기능 단위테스트")
 public class JoinServiceTest {
+
     private JoinService joinService;
 
     @BeforeEach
     void init() {
-        //joinService = new JoinService(new JoinValidator(), new MemberDao()); // 관리하기 편한 객체조립기 필요함
         joinService = ServiceManager.getInstance().joinService();
     }
 
     private Member getMember() {
+        String userPw = "12345678";
         return Member.builder()
                 .userId("user" + System.currentTimeMillis())
-                .userPw("12345678")
-                .confirmUserPw("12345678")
+                .userPw(userPw)
+                .confirmUserPw(userPw)
                 .userNm("사용자")
                 .email("user@test.org")
                 .agree(true)
                 .build();
-
     }
+
     @Test
     @DisplayName("회원가입 성공시 예외발생하지 않음")
     void joinSuccess() {
         assertDoesNotThrow(() -> {
             joinService.join(getMember());
         });
-
     }
 
     @Test
-    @DisplayName("필수 항목 검증(아이디, 비밀번호, 비밀번호 확인, 회원명, 이메일, 회원가입약관 동의)")
+    @DisplayName("필수 항목 검증(아이디, 비밀번호, 비밀번호 확인, 회원명, 이메일, 회원가입약관 동의), 검증 실패시 BadRequestException 발생")
     void requiredFieldCheck() {
-        //아이디(userId가 null 또는 빈값("")
-        assertThrows(BadRequestException.class, () -> {
+        // 아이디(userId)가 null 또는 빈값("")
+        BadRequestException thrown = assertThrows(BadRequestException.class, () -> {
             Member member = getMember();
 
             member.setUserId(null);
             joinService.join(member);
 
-            member.setUserId("    ");
+            member.setUserId("   ");
             joinService.join(member);
         });
+
+        String message = thrown.getMessage();
+        //assertEquals("아이디를 입력하세요.", message);
+        assertTrue(message.contains("아이디"));
+
+        // 비밀번호(userPw)가 null 또는 빈값("")
+        assertThrows(BadRequestException.class, () -> {
+            Member member = getMember();
+
+            member.setUserPw(null);
+            joinService.join(member);
+
+            member.setUserId("   ");
+            joinService.join(member);
+        });
+    }
+
+    private void requiredFieldEachCheck(Member member, String word) {
+        BadRequestException thrown = assertThrows(BadRequestException.class, () -> {
+            joinService.join(member);
+        });
+        assertTrue(thrown.getMessage().contains(word));
     }
 }
